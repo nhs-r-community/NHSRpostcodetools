8e9b879f84142a49743eace6effd11269fb5f702
Fran Barton <francis.barton@nuh.nhs.uk>
2024-01-26 08:44:20 +0000

    Tidy up package documentation - remove unnecessary function imports

--- R/NHSRpostcodetools-package.R
+++ R/NHSRpostcodetools-package.R
@@ -3,15 +3,10 @@
 
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
-#' @importFrom dplyr across c_across desc join_by
-#' @importFrom magrittr %>%
-#' @importFrom rlang `:=`
-#' @importFrom tidyselect all_of any_of contains ends_with everything
-#' @importFrom tidyselect last_col matches num_range starts_with
-#' @importFrom usethis ui_info ui_stop ui_oops ui_nope ui_code
-
-# Borrow `view()` from `{tibble}`
-#' @export
-view <- tibble::view
+#' @importFrom dplyr across
+#' @importFrom rlang .data `:=`
+#' @importFrom tidyselect all_of
+#' @importFrom usethis ui_info ui_nope ui_oops ui_stop
+#' @importFrom utils URLencode
 
 NULL
